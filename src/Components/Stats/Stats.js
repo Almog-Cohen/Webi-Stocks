b410759865adc750675b15f4c1931c6104e4cc1d
Almog Cohen <almogco94@gmail.com>
2021-01-06 11:02:03 +0200

    Fetching stocks data

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Stats.css";
+import StockRow from "../StockRow/StockRow";
 
 const Stats = () => {
+  // const TOKEN = "bvqau6n48v6s3bgpkqag";
+  const STOCK_URL = "https://finnhub.io/api/v1/quote";
+  const [stockData, setStockData] = useState([]);
+  const [seconds, setSeconds] = useState(0);
+  const getStockData = (stock) => {
+    return axios
+      .get(`${STOCK_URL}?symbol=${stock}&token=${process.env.REACT_APP_TOKEN}`)
+      .catch((err) => console.log(err));
+  };
+
+  setInterval(() => {
+    setSeconds((seconds) => seconds + 1);
+  }, 5000);
+
+  useEffect(() => {
+    let tempStockData = [];
+    console.log("shit happns");
+    // const interval = setInterval(() => {
+    //   setTimers((timer) => timer + 1);
+    // }, 1000);
+
+    const stocksList = ["AAPL", "MSFT", "TSLA", "FB", "BABA", "UBER"];
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStockData(stock).then((res) => {
+          tempStockData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      setStockData(tempStockData);
+      console.log(tempStockData);
+    });
+
+    // return () => clearTimeout(interval);
+  }, []);
+
   return (
     <div className="stats">
       <div className="stats__container">
@@ -9,14 +53,23 @@ const Stats = () => {
           <p>Stocks</p>
         </div>
         <div className="stats__content">
-          <div className="stats__stocks"></div>
+          <div className="stats__rows">
+            {stockData.map((stock) => (
+              <StockRow
+                key={stock.name}
+                name={stock.name}
+                openPrice={stock.o}
+                price={stock.c}
+              />
+            ))}
+          </div>
         </div>
         <div className="stats__header">
           <p>Lists</p>
         </div>
         {/* current stocks */}
         <div className="stats__content">
-          <div className="stats__stocks">{/* stocks we buy  */}</div>
+          <div className="stats__rows">{/* stocks we buy  */}</div>
         </div>
       </div>
     </div>
